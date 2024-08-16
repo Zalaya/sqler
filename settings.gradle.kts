c104d9e679d4352127f4814ccfd72f41ab8180f1
zalaya <enol.corzal@gmail.com>
2024-08-16 16:54:11 +0200

    feat(rest-api): add the rest api modules and submodules, it will have an hexagonal architecture with a starting point of three layers, application, domain and infrastructure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "sqler"
+
+include("sqler-rest-api")
+
+include(
+    "sqler-rest-api:sqler-rest-api-application",
+    "sqler-rest-api:sqler-rest-api-domain",
+    "sqler-rest-api:sqler-rest-api-infrastructure",
+)
