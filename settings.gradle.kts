45845020b33e4b23954d4174adf8fc569a7afc71
zalaya <enol.corzal@gmail.com>
2024-08-16 17:02:06 +0200

    refactor(modules): remove the unused modules till we have a new structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
 rootProject.name = "sqler"
-
-include("sqler-rest-api")
-
-include(
-    "sqler-rest-api:sqler-rest-api-application",
-    "sqler-rest-api:sqler-rest-api-domain",
-    "sqler-rest-api:sqler-rest-api-infrastructure",
-)
