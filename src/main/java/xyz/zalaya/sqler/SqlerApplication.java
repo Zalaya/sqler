565bb595600a02997e34c191c99f718ebf81f9ef
zalaya <enol.corzal@gmail.com>
2024-08-26 21:46:38 +0200

    feat(structure): change to a spring boot structure and architecture, we will migrate it later
    # Conflicts:
    #	.gitignore
    #	pom.xml

---
+++ src/main/java/xyz/zalaya/sqler/SqlerApplication.java
@@ -0,0 +1,13 @@
+package xyz.zalaya.sqler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/zalaya/sqler/SqlerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.zalaya.sqler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
