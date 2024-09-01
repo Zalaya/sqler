ea40f06971161d040346d204ab0c72e423095bb6
zalaya <enol.corzal@gmail.com>
2024-09-01 21:31:07 +0200

    feat(models): create the new structure as the domain is already modeled

--- src/shared/main/java/domain/exception/InvalidStringValueException.java
+++
@@ -1,9 +0,0 @@
-package shared.main.java.domain.exception;
-
-public class InvalidStringValueException extends RuntimeException {
-
-    public InvalidStringValueException(String value) {
-        super(String.format("The value <%s> is not a valid string", value));
-    }
-
-}
